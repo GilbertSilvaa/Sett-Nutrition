cb885c6aa30487366d573bca916525fb984a9b60
gilbertsilvaa <kemegil515@gmail.com>
2023-07-10 21:33:53 -0300

    feat: I placed the panel, the food consumption box and the footer with the save and cancel buttons on the meal screen.

--- mobile/src/screens/MealScreen/index.tsx
+++ mobile/src/screens/MealScreen/index.tsx
@@ -1,6 +1,38 @@
 import { useRoute, RouteProp } from '@react-navigation/native';
 import { RootStackParamList } from '../../routes/stack-routes';
+
+import { BoxConsumption } from '../../components/BoxConsumption';
+import { Panel } from '../../components/Panel';
+
 import { Container, Header, Text } from '../Meals/styles';
+import { Footer, Main } from './styles';
+import { ButtonOption } from '../../components/ButtonOption';
+
+const amountMacrosExample = [
+  { 
+    amount: 900,
+    macroName: 'carbs',
+    color: '#00fff0'
+  },
+  { 
+    amount: 250,
+    macroName: 'protein',
+    color: '#ff5656'
+  },
+  { 
+    amount: 335,
+    macroName: 'gord',
+    color: '#ffb673'
+  },
+];
+const caloriesExample = {
+  amount: 205,
+  goal: 3000
+}
+const waterExample = {
+  amount: 900,
+  goal: 2500
+}
 
 type MealScreenRouteProps = RouteProp<RootStackParamList, 'Meal'>;
 
@@ -12,6 +44,21 @@ export function MealScreen() {
       <Header>
         <Text>{params.nameMeal}</Text>
       </Header>
+      <Main>
+        <Panel 
+          macros={amountMacrosExample}
+          calories={caloriesExample}
+          water={waterExample}
+        />
+
+        <BoxConsumption title="Alimentos" iconName="pizza-outline">
+
+        </BoxConsumption>
+      </Main>
+      <Footer>
+        <ButtonOption title="cancelar" color="#b32b2b"/>
+        <ButtonOption title="salvar" color="#2d9450"/>
+      </Footer>
     </Container>
   );
 }
---
+++ mobile/src/screens/MealScreen/styles.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const Main = styled.ScrollView`
+  width: 100%;
+  height: 100%;
+  padding: 0 16px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  padding: 12px;
+  background-color: #257be2;
+  border-top-right-radius: 8px;
+  border-top-left-radius: 8px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
